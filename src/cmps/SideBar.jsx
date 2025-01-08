39866315db9c4693e2e2aba3bedb3f49c56653ed
supaGirl <kerenvasserman.bm@gmail.com>
2025-01-08 20:33:28 +0200

    Sidebar style change

--- src/cmps/SideBar.jsx
+++ src/cmps/SideBar.jsx
@@ -2,66 +2,53 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export function SideBar() {
+  function goToBoard() {
+   <Link to="/board"></Link>
+  }
+  const boards = boardService.getRamdonBoards()
   return (
     <div className="board-sidebar">
+      <header className="sidebar-header">
+        <div className="sidebar-logo" onClick={goToBoard}>
+          <span className="sidebarlogo">D</span>
+        </div>{' '}
+        <p className="logo-link" onClick={goToBoard}>
+          Donotello Workspace
+        </p>
+      </header>
+
       <ul className="sidebar-links">
-        <li >
-          <Link to="/boards">
+        <li className="main-board">
+          <Link to="/board">
             <div></div>
             <span>Boards</span>
           </Link>
         </li>
-        <li>
-          <Link to="/templates">
+        <li className="members">
+          <Link to="/members">
             <div></div>
-            <p>Templates</p>
+            <p>Members</p>
           </Link>
         </li>
-        <li>
-          <Link to="/home">
-            <div></div>
-            <p>Home</p>
-          </Link>
-        </li>
-        <div className="saparete"></div>
-        <p>Workspace</p>
-        <div className="sidebar-bomerang">
-          <h6>
-           
-            <div className="sidebar-logo">
-              <div className="sidebarlogo">D</div>
-            </div>{' '}
-            Donotello Workspace <span className="bomerang-icon">🇻</span>
-          </h6>
-          <li className="active">
-            <Link to="/boards">
-              <div></div> <p>Boards</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/collections">
-              <div></div> <p>Collections</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/highlights">
-              <div></div>
-              <p>Highlights</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/members">
-              <div></div>
-              <p>Members</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/settings">
-              <div></div>
-              <p>Settings</p>
-            </Link>
-          </li>
+        <div className="boards-action">
+          <h2 className="title">Your boards</h2>
+          <div className="btn-actions">
+            <button className="sort-by">...</button>
+            <button className="add-board">+</button>
+          </div>
         </div>
+        {boards.map((board) => (
+          <li key={board._id} className="boards-list">
+            <Link to={`/board/${board._id}`}>
+              <div></div>
+              <span>{board.title}</span>
+            </Link>
+            <div className="btn-actions">
+              <button className="sort-by">...</button>
+              <button className="star-board">⭐</button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   )
