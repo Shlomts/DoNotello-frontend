552f441d7ab3b7ad6ad285b6217fcc63847ccb7c
chen <levavi.chen@gmail.com>
2025-01-24 23:30:53 +0200

    add rotate to d&d

--- src/cmps/card/CardList.jsx
+++ src/cmps/card/CardList.jsx
@@ -6,17 +6,47 @@ import { Droppable, Draggable } from 'react-beautiful-dnd'
 export function CardList({ cards, board, group }) {
     if (!cards || cards.length === 0) return null
 
+    function getStyle(style, snapshot) {
+        if (snapshot.isDragging) {
+            return {
+                ...style,
+                transform: `${style?.transform || ""} rotate(5deg)`,
+                transition: "transform 0.2 ease",
+                zIndex: 10,
+            }
+        }
+
+        if (snapshot.isDropAnimating) {
+            const { moveTo, curve, duration } = snapshot.dropAnimation
+            const translate = `translate(${moveTo.x}px, ${moveTo.y}px)`
+
+            return {
+                ...style,
+                transform: translate,
+                transition: `all ${curve} ${duration}s`,
+            };
+        }
+
+        return style
+    }
+
     return (
         <Droppable droppableId={group.id} direction='vertical' type='card'>
-            {(provided) => (
-                <ul className="card-list" {...provided.droppableProps} ref={provided.innerRef}>
+            {(provided, snapshot) => (
+                <ul className=
+                    {`card-list ${snapshot.isDraggingOver ? "dragging-over" : ""}`}
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}>
                     {cards.map((card, index) => (
                         <Draggable key={card.id} draggableId={card.id} index={index} type='card'>
-                            {(provided) => (
+                            {(provided, snapshot) => (
                                 <li
                                     ref={provided.innerRef}
                                     {...provided.draggableProps}
                                     {...provided.dragHandleProps}
+                                    style={getStyle(provided.draggableProps.style, snapshot)}
+                                    className={`draggable-container ${snapshot.isDragging ? "isDragging" : ""}`}
+
                                 >
                                     <CardPreview
                                         card={card}
--- src/cmps/group/GroupList.jsx
+++ src/cmps/group/GroupList.jsx
@@ -1,36 +1,73 @@
+import { useRef } from "react"
 import { GroupDetails } from "../group/GroupDetails.jsx"
-import { DragDropHandler } from "../DragDropHandler.jsx"
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 
 
 export function GroupList({ board, groups, onRemoveGroup }) {
     if (!groups || groups.length === 0) return null
 
+    function getStyle(style, snapshot, elementRef) {
+        if (snapshot.isDragging && elementRef?.current) {
+            const actualHeight = elementRef.current.offsetHeight
+            // console.log('actual height:', actualHeight)
+            return {
+                ...style,
+                transform: `${style?.transform || ""} rotate(5deg)`,
+                height: `${actualHeight}px`,
+                transition: "transform 0.2 ease",
+                zIndex: 10,
+            }
+        }
+
+        if (snapshot.isDropAnimating) {
+            const { moveTo, curve, duration } = snapshot.dropAnimation
+            const translate = `translate(${moveTo.x}px, ${moveTo.y}px)`
+
+            return {
+                ...style,
+                transform: translate,
+                transition: `all ${curve} ${duration}s`,
+            };
+        }
+
+        return style
+    }
+
     return (
         <Droppable droppableId="group-list" direction="horizontal" type="group">
-            {(provided) => (
-                <ul className="group-list" {...provided.droppableProps} ref={provided.innerRef}>
-                    {groups.map((group, index) => (
-                        <Draggable key={group.id} draggableId={group.id} index={index} type='group' >
-                            {(provided) => (
-                                < li
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                >
-                                    <GroupDetails
-                                        board={board}
-                                        group={group}
-                                        onRemoveGroup={onRemoveGroup}
-                                    />
-                                </li>
-                            )}
-                        </Draggable>
-                    ))}
+            {(provided, snapshot) => (
+                <ul className=
+                    {`group-list ${snapshot.isDraggingOver ? "dragging-over" : ""}`}
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}>
+                    {groups.map((group, index) => {
+                        const elementRef = useRef(null)
+                        return (
+                            <Draggable key={group.id} draggableId={group.id} index={index} type='group' >
+                                {(provided, snapshot) => (
+                                    < li
+                                        ref={(ref) => {
+                                            provided.innerRef(ref)
+                                            elementRef.current = ref
+                                        }}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        style={getStyle(provided.draggableProps.style, snapshot, elementRef)}
+                                        className={`draggable-container ${snapshot.isDragging ? "isDragging" : ""}`}
+                                    >
+                                        <GroupDetails
+                                            board={board}
+                                            group={group}
+                                            onRemoveGroup={onRemoveGroup}
+                                        />
+                                    </li>
+                                )}
+                            </Draggable>
+                        )
+                    })}
                     {provided.placeholder}
                 </ul>
-            )
-            }
+            )}
         </Droppable >
     )
 }
