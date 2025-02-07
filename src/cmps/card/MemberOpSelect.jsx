0c07129c65f5cd64f3365b4d770c7732f5552435
supaGirl <kerenvasserman.bm@gmail.com>
2025-02-07 20:02:28 +0200

    Fix filter padding and svg

--- src/cmps/card/MemberOpSelect.jsx
+++ src/cmps/card/MemberOpSelect.jsx
@@ -1,6 +1,6 @@
 import {Avatar, Checkbox} from '@mui/material'
 import {useEffect, useState} from 'react'
-import {DownArrow} from '../SvgContainer'
+import {DownArrow, SmallDownArrow} from '../SvgContainer'
 
 export function MemeberOpSelect({members, selectedMemberIds, onSelect}) {
   const [open, setOpen] = useState(false)
@@ -20,11 +20,11 @@ export function MemeberOpSelect({members, selectedMemberIds, onSelect}) {
 
   return (
     <div className="custom-select">
-      <div className="select-trigger" onClick={() => setOpen(!open)}>
+      <div className={`select-trigger ${open ? 'active' : ''}`} onClick={() => setOpen(!open)}>
         {selectedMembers.length > 0 ? `${selectedMembers.length} selected` : 'Select members'}
 
         <span className="icon">
-          <DownArrow />
+          <SmallDownArrow />
         </span>
       </div>
 
@@ -33,15 +33,14 @@ export function MemeberOpSelect({members, selectedMemberIds, onSelect}) {
           {members.map((member) => (
             <div key={member._id} className="dropdown-item" onClick={() => handleSelect(member)}>
               <Checkbox
-                className="checkbox"
+                className="checkbox-icon"
                 value={member.id}
-
                 checked={selectedMembers.includes(member._id)}
                 onChange={() => handleSelect(member._id)}
                 sx={{
                   color: '#738496',
-                  width: '16px',
-                  height: '16px',
+                     height:'10px',
+                      width:'10px',
                   '&.Mui-checked': {color: '#579dff'},
                 }}
               />
